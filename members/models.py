4d1aa104eff8d59ffda54dfd6402e1e70112a8b6
Daniel Nery <danielnso@outlook.com>
2018-03-03 18:46:08 +0000

    :card_file_box: Completed members db model

--- members/models.py
+++ members/models.py
@@ -1,9 +1,49 @@
-from django.db import models
+"""
+ThunderSite - ThundeRatz
 
-# Create your models here.
+Members Models
+Daniel Nery Silva de Oliveira
+
+02/2018
+"""
+
+from django.db import models
+from django.core.exceptions import ValidationError
+
+def member_picture_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return 'members/' + instance.nickname + '.' + ext
+
 class Member(models.Model):
+    CHOICES = (
+        ('EL', 'Elétrica'),
+        ('MC', 'Mecânica'),
+        ('CP', 'Computação'),
+        ('DM', 'Design e Marketing'),
+        ('AF', 'Administrativo e Financeiro'),
+    )
+
     name = models.CharField(max_length=128)
-    nickname = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=64, unique=True)
+    area = models.CharField(max_length=2, choices=CHOICES)
+    area_captain = models.BooleanField(default=False)
+    team_captain = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to=member_picture_path)
+    facebook = models.URLField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['team_captain', 'area_captain', 'name']
 
     def __str__(self):
         return self.name
+
+    def clean(self):
+        if self.area_captain is True and self.team_captain is True:
+            raise ValidationError({
+                'area_captain': ValidationError(_('Choose only one!.'), code='invalid'),
+                'team_captain': ValidationError(_('Choose only one!'), code='invalid'),
+            })
+        if self.area_captain is True and self.area == 'AF':
+            raise ValidationError({
+                'area_captain': ValidationError(_("This area doesn't have captains!"), code='invalid'),
+            })
