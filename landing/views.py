24c321cc7e14743139e5b96b337b626b6693f6db
Felipe Gomes de Melo <felipe.gomes.de.melo@energy-robotics.com>
2022-01-24 23:49:59 -0300

    ✨  Parametrize donation state and goal

--- landing/views.py
+++ landing/views.py
@@ -9,7 +9,7 @@ Daniel Nery Silva de Oliveira
 
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, DetailView
-from .models import Event, Sponsor, About, Recruitment
+from .models import Event, Sponsor, About, Recruitment, ProgressBar
 from news.models import News
 
 
@@ -24,16 +24,17 @@ class IndexView(TemplateView):
         context["news"] = News.objects.all()[:3]
         context["the_team"] = About.objects.get(type="A")
         context["workshop"] = About.objects.get(type="B")
+        context["thunder_donations"] = ProgressBar.objects.get(name="thunder_donations")
 
         return context
+
 
 class RecruitmentView(TemplateView):
     template_name = "landing/recruitment.html"
 
     def get_context_data(self, **kwargs):
         context = super(RecruitmentView, self).get_context_data(**kwargs)
-        context['recruitment'] = Recruitment.objects.all()[0]
+        context["recruitment"] = Recruitment.objects.all()[0]
 
         return context
-
 
