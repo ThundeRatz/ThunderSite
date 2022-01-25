36efa10934380105c195c629a0418837a09ef204
Felipe Gomes de Melo <felipe.gomes.de.melo@energy-robotics.com>
2022-01-24 22:43:42 -0300

    🎨  Format local_settings.docker.py

--- local_settings.docker.py
+++ local_settings.docker.py
@@ -2,11 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['0.0.0.0']
-MEDIA_ROOT = os.path.join(BASE_DIR, 'wget/thunderatz.org/media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'wget/thunderatz.org/static/')
+ALLOWED_HOSTS = ["0.0.0.0"]
+MEDIA_ROOT = os.path.join(BASE_DIR, "wget/thunderatz.org/media/")
+STATIC_ROOT = os.path.join(BASE_DIR, "wget/thunderatz.org/static/")
 
-SECRET_KEY = 'essa_eh_a_chave_secreta'
+SECRET_KEY = "essa_eh_a_chave_secreta"
 
 VENV_ROOT = "/usr/local"
 DEBUG = True
@@ -18,12 +18,12 @@ COMPRESS_ENABLED = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'thundersite-database',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "thundersite-database",
+        "PORT": "5432",
     }
 }
